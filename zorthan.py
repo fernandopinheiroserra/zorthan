da38284ca8b1aa9cb43e186f4cfbe21f1430c1f4
fernandopinheiroserra <fernando.serra@alternativasistemas.com.br>
2022-03-02 13:19:39 -0300

    Create zorthan.py
    Zorthan, o biscoito da sorte amaldiçoada sabe a verdade!
    
    Zorthan, o mestre das probabilidades, está de volta!
    
    *random.choice

---
+++ zorthan.py
@@ -0,0 +1,17 @@
+import random
+
+sorte = [
+    'Tudo começa a dar certo, quando deixa de dar errado', 
+    'Nenhum obstáculo é grande demais para quem desiste', 
+    'Sem lutas não há derrotas!', 
+    'Você não pode mudar seu passado, mas pode estragar seu futuro!', 
+    'Lute como nunca, perca como sempre',
+    'Não sabendo que era impossível, corra atrás e saiba',
+    'Nada é tão horrível que não possa piorar muito',
+    'Se alguém te ofendeu sem você merecer, volte lá e mereça!',
+    'Você é mais fraco do que pensa',
+    'O caminho é longo, mas a derrota é certa']
+zguess = random.choice(sorte)
+
+print('Bem-vindo mortal!!Eu sou Zorthan, o biscoito da sorte amaldiçoado!! Leia sua sorte agora e ilumine sua vida!!')
+print(f'Seu pedaço de sabedoria cósmica de hoje é: {zguess}')
